0b65c1f40585c80fa9e2195c4629be27b4098f16
quwps <nicholasstern98@gmail.com>
2025-03-23 17:26:27 -0400

    f

--- js/layers/prestige.js
+++ js/layers/prestige.js
@@ -34,10 +34,10 @@ addLayer("p", {
         "buyables",
         "blank",
     ],
-    color: "#4BDC13",
+    color: "#ffb605",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "Pens", // Name of prestige currency
+    baseResource: "Pencils", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent() {
