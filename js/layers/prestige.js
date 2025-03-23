9990ac0db745471b1e40f68d5881d867234770b2
quwps <nicholasstern98@gmail.com>
2025-03-23 19:53:02 -0400

    Revert "f"
    This reverts commit 24403fb360608cd46f3e5c794f0668c1e0edb31c.

--- js/layers/prestige.js
+++ js/layers/prestige.js
@@ -34,7 +34,7 @@ addLayer("p", {
         "buyables",
         "blank",
     ],
-    color: "#03dbfc",
+    color: "#7CB9E8",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Pens", // Name of prestige currency
     baseResource: "Pencils", // Name of resource prestige is based on
@@ -84,7 +84,6 @@ addLayer("p", {
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "p", description: "P: Reset for Pens", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-        branches, ["k"]
     ],
     layerShown(){return true},
     upgrades:{
